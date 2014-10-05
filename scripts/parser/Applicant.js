8f8ab48f17d93cab0db2d7c80c7b9df287dec877
Eric <wang.eric.r@gmail.com>
2014-10-04 20:54:24 -0400

    made Parser object with parse method

--- scripts/parser/Applicant.js
+++ scripts/parser/Applicant.js
@@ -1,17 +1,20 @@
 define([], function(){ 
   var Application = function(){
+	this.fields = ["Name:", "DOB:", "Resume:"];
     this.name = "";
     this.DOB = "";
     this.resume = ""; 
 
-    function getName(){ return this.name; }
-    function getDOB(){ return this.DOB; }
-    function getResume(){ return this.resume; }
+    function getName(){ return name; }
+    function getDOB(){ return DOB; }
+    function getResume(){ return resume; }
+	function getFields(){ return fields; }
     
     return {
       getName: getName,
       getDOB: getDOB,
-      getResume: getResume
+      getResume: getResume,
+	  getFields: getFields
     };
   };
 
--- scripts/parser/parser.js
+++ scripts/parser/parser.js
@@ -1,42 +1,44 @@
 require(['Applicant'], function(Applicant){
-  $(document).ready(function(){
-    var field_names = ["NAME:", "ID:", "DOB:"];
-    
-    //var person = new Applicant();
-    
-    //start temp
-    var person = new Object;
-    for(var i = 0, len = field_names; i < len; i++){
-      person[field_names[i]] = "";
-    }
-    //end temp
+  var Parser = {
+	parse: function(myText){
+	  $(document).ready(function(){
+		//var field_names = ["NAME:", "ID:", "DOB:"];
+	 
+		var person = Applicant();
+		
+		var field_names = person.getFields();
+		
+		/*
+		//start temp
+		var person = new Object;
+		for(var i = 0, len = field_names; i < len; i++){
+		  person[field_names[i]] = "";
+		}
+		//end temp
+		*/
 
-    var myText = "asdfasdfasdf NAME:Eric \n Wang \n tester \n ID:1234 \n DOB:12";
+		//var myText = "asdfasdfasdf Name:Eric \n Wang \n DOB:12 \n Resume: 1234";
 
-    var lines = myText.split("\n"),
-      field_contents = [], tempVar;
+		var lines = myText.split("\n"), tempVar;
 
-    j = 0
-    current_name = "";
-    for(var i = 0, len = lines.length; i < len; i++){
-      tempVar = lines[i].indexOf(field_names[j]);
-      if(tempVar !== -1){
-        person[field_names[j]] = lines[i].substring(tempVar+field_names[j].length);
-        current_name = field_names[j];
-        j += 1;
-      }
-      else if(current_name !== ""){
-        person[field_names[j-1]] += " " + lines[i];
-      }	
-    }
-    
-    var tempStr = "";
-    for(var property in person){
-      if(person.hasOwnProperty(property)){
-        tempStr += person[property];
-      }
-    }
-    
-    document.getElementById("edit").innerHTML = tempStr;
-  });
+		j = 0
+		current_name = "";
+		for(var i = 0, len = lines.length; i < len; i++){
+		  tempVar = lines[i].indexOf(field_names[j]);
+		  if(tempVar !== -1){
+			person[field_names[j]] = lines[i].substring(tempVar+field_names[j].length);
+			current_name = field_names[j];
+			j += 1;
+		  }
+		  else if(current_name !== ""){
+			person[field_names[j-1]] += " " + lines[i];
+		  }	
+		}
+		
+		var tempStr = "";
+		
+		return person;
+		});
+	}
+	}
 }); 
